ce275af881ee6d6c5eebba7377404a574c6b6b7a
PikaSannnnn <Dong.Stephen11@gmail.com>
2021-04-17 11:04:54 -0700

    Update part 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: sdong027
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #4 Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,11 +13,48 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+  	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFE; PORTB = 0x00; // ooo oooi 
+    unsigned char tmpA = 0x00;
+	unsigned char tmpB = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
+    enum LED {OFF, OFF_WAIT, ON, ON_WAIT} LED_STATE;
 
-    }
-    return 1;
+	while(1) {
+        tmpA = PINA & 0x03;
+        LED_STATE = OFF_WAIT;
+        
+        switch (LED_STATE) {
+            case OFF:
+                tmpB = 0x01;
+                if (!(tmpA & 0x01)) {
+                    LED_STATE = OFF_WAIT;
+                }
+                break;
+            case OFF_WAIT:
+                tmpB = 0x01;
+                if (tmpA & 0x01) {
+                    LED_STATE = ON;
+                }
+                break;
+            case ON:
+                tmpB = 0x02;
+                if (!(tmpA & 0x01)) {
+                    LED_STATE = ON_WAIT;
+                }
+                break;
+            case ON_WAIT:
+                tmpB = 0x02;
+                if (tmpA & 0x01) {
+                    LED_STATE = OFF;
+                }
+                break;
+            default:
+                LED_STATE = OFF_WAIT;
+                break;
+        }
+
+        PORTB = tmpB;
+	}
+	return 0;
 }
--- test/tests.py
+++ test/tests.py
@@ -14,21 +14,32 @@
 # An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
-tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',<val>)], 'iterations': 1 } ],
-    'expected': [('PORT',<val>)],
+# Test sequence from waitA0: A0, !A0, A1 => PORTB: 0xF0
+tests = [ {'description': 'PINA: 0x01, 0x00 (press) => PORTB: 2, state: ON_WAIT',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x00)], 'iterations': 2}],
+    'expected': [('PORTB',0x02)],
     },
-    {'description': 'This test will run second.',
-    'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
-        {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
-        {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
-        {'inputs': [('PIN',<val>)], 'time': 600}, ],
-    'expected': [('PORT',<val>)],
+    {'description': 'PINA: 0x01 (HOLD) => PORTB: 2, state: ON',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 20}],
+    'expected': [('PORTB',0x02)],
+    },
+    {'description': 'PINA: 0x00 (No press) => PORTB: 1, state: OFF_WAIT',
+    'steps': [{'inputs': [('PINA', 0x00)], 'iterations': 10}],
+    'expected': [('PORTB',0x01)],
+    },
+    {'description': 'PINA: 0x01, 0x00, 0x01, 0x00 (2x (with hold) press) => PORTB: 1, state: OFF_WAIT',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 20},
+        {'inputs': [('PINA', 0x00)], 'iterations': 2},
+        {'inputs': [('PINA', 0x01)], 'iterations': 20},
+        {'inputs': [('PINA', 0x00)], 'iterations': 2}],
+    'expected': [('PORTB',0x01)],
     },
     ]
+
 
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['<function>::<static-var>','PORTB']
+watch = ['state']
 
