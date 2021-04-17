595f3c5b333b84357446167ce8417f012b0c6cc9
PikaSannnnn <Dong.Stephen11@gmail.com>
2021-04-17 15:47:01 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,3 +1,42 @@
+// /*	Author: sdong027
+//  *  Partner(s) Name: 
+//  *	Lab Section:
+//  *	Assignment: Lab #4 Exercise #1
+//  *	Exercise Description: [optional - include for your own benefit]
+//  *
+//  *	I acknowledge all content contained herein, excluding template or example
+//  *	code, is my own original work.
+//  */
+// #include <avr/io.h>
+// #ifdef _SIMULATE_
+// #include "simAVRHeader.h"
+// #endif
+
+// int main(void) {
+//   	DDRA = 0x00; PORTA = 0xFF;
+// 	DDRC = 0xFE; PORTC = 0x00; // ooo oooi 
+//     unsigned char tmpA = 0x00;
+// 	unsigned char tmpC = 0x00;
+
+//     enum OUT {INIT, INC, DEC} OUT_STATE;
+//     OUT_STATE = INIT;
+// 	while(1) {
+//         tmpA = PINA & 0x03;
+        
+//         switch (OUT_STATE) {
+//             case INIT:
+//                 tmpC = 0x07;
+//                 break;
+//             default:
+//                 LED_STATE = OFF_WAIT;
+//                 break;
+//         }
+
+//         PORTB = tmpB;
+// 	}
+// 	return 0;
+// }
+
 /*	Author: sdong027
  *  Partner(s) Name: 
  *	Lab Section:
@@ -14,18 +53,58 @@
 
 int main(void) {
   	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFE; PORTC = 0x00; // ooo oooi 
+	DDRB = 0xFE; PORTB = 0x00; // ooo oooi 
     unsigned char tmpA = 0x00;
-	unsigned char tmpC = 0x00;
+	unsigned char tmpB = 0x00;
 
-    enum OUT {INIT, INC, DEC} OUT_STATE;
-    OUT_STATE = INIT;
+    enum LED {INIT, OFF, OFF_WAIT, ON, ON_WAIT} LED_STATE;
+    LED_STATE = OFF_WAIT;
 	while(1) {
         tmpA = PINA & 0x03;
         
-        switch (OUT_STATE) {
+        switch (LED_STATE) {
             case INIT:
-                tmpC = 0x07;
+                tmpB = 0x01;
+                break;
+            case OFF:
+                if (!(tmpA)) {
+                    LED_STATE = OFF_WAIT;
+                }
+                break;
+            case OFF_WAIT:
+                if (tmpA) {
+                    tmpB = 0x02;
+                    LED_STATE = ON;
+                }
+                break;
+            case ON:
+                if (!(tmpA)) {
+                    LED_STATE = ON_WAIT;
+                }
+                break;
+            case ON_WAIT:
+                if (tmpA) {
+                    tmpB = 0x01;
+                    LED_STATE = OFF;
+                }
+                break;
+            default:
+                LED_STATE = OFF_WAIT;
+                break;
+        }
+
+        switch (LED_STATE) {
+            case OFF:
+                tmpB = 0x01;
+                break;
+            case OFF_WAIT:
+                tmpB = 0x01;
+                break;
+            case ON:
+                tmpB = 0x02;
+                break;
+            case ON_WAIT:
+                tmpB = 0x02;
                 break;
             default:
                 LED_STATE = OFF_WAIT;
